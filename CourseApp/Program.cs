84ac479011d81b25a376680e09731e99a5b54a02
Acerlaorion <ilyamaksakov123@gmail.com>
2019-10-14 17:52:16 +0300

    Задание А и В без тестов

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -6,13 +6,21 @@ namespace CourseApp
     {
         public static double MyFunction(double a, double b, double x)
         {
-            var y = (a * Math.Pow(x, 2)) + (b * x);
+            var y = (1 + Math.Pow(Math.Log10(x / a), 2)) / (b - Math.Exp(x / a));
             return y;
         }
 
         public static double[] TaskA(double a, double b, double xn, double xk, double dx)
         {
-            return new double[0];
+            int i = 0;
+            var y = new double[(int)((xk - xn) / dx)];
+            for (double x = xn; x < xk; x += dx)
+            {
+                y[i] = MyFunction(a, b, x);
+                i++;
+            }
+
+            return y;
         }
 
         public static double[] TaskB(double a, double b, double[] x)
@@ -28,14 +36,20 @@ namespace CourseApp
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            const double a = 2.2;
-            const double b = 3.8;
-            var resSingle = MyFunction(a, b, 4);
-            Console.WriteLine(resSingle);
-            var x = new double[] { 1, 2, 3, 4, 5 };
-            var taskBRes = TaskB(a, b, x);
-            foreach (var item in taskBRes)
+            const double a = 2.0;
+            const double b = 0.95;
+            const double xn = 1.25;
+            const double xk = 2.75;
+            const double dx = 0.3;
+            Console.WriteLine("Задание А:");
+            foreach (var item in TaskA(a, b, xn, xk, dx))
+            {
+                Console.WriteLine($"y = {item}");
+            }
+
+            Console.WriteLine("Задание B:");
+            var x = new double[] { 2.2, 3.78, 4.51, 6.58, 1.2 };
+            foreach (var item in TaskB(a, b, x))
             {
                 Console.WriteLine($"y = {item}");
             }
