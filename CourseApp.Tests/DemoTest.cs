87d0086bd4850c3de8a0a1da469012eb658072ba
viktormurashov <murashovviktorv@gmail.com>
2019-10-28 09:23:22 +0400

    NOT FULL VERSION

--- CourseApp.Tests/DemoTest.cs
+++ CourseApp.Tests/DemoTest.cs
@@ -12,17 +12,28 @@ namespace CourseApp.Tests
         }
 
         [Fact]
-        public void TestSummPositive()
+        public void TestMyFunctionZeros()
         {
-            var res = Program.Summ(2, 3);
-            Assert.Equal(5, res);
+            var res = Program.MyFunction(0.0,0.0,0.0);
+            Assert.Equal(double.NaN, res);
         }
 
         [Fact]
-        public void TestSummPositiveNegative()
+        public void TestTaskBNullMass()
         {
-            var actualRes = Program.Summ(-2, 2);
-            Assert.Equal(0, actualRes);
+            var mass = new double[0];
+            var res = Program.TaskB(1,2,mass);
+            Assert.Equal(mass,res);
         }
+        [Fact]
+        public void TestTaskB()
+        {
+            var x = new double[] { 1.88, 2.26, 3.84, 4.55, -6.21 };
+            var res = Program.TaskB(1,2,x);
+            Assert.Equal(x,res);
+        }
+        
+     
+
     }
 }
