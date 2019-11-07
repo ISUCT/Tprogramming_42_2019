39242c8b52b45ae7f64f25705ad37096b68dfe08
Acerlaorion <ilyamaksakov123@gmail.com>
2019-11-07 21:22:37 +0300

    Fix error

--- CourseApp.Tests/DemoTest.cs
+++ CourseApp.Tests/DemoTest.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using NUnit.Framework;
+
 namespace CourseApp.Tests
 {
     public class DemoTest
@@ -14,7 +15,7 @@ namespace CourseApp.Tests
         [Fact]
         public void TestMyFunctionZeros()
         {
-            var res = Program.MyFunction(0.0,0.0,0.0);
+            var res = Program.MyFunction(0.0, 0.0, 0.0);
             Xunit.Assert.Equal(double.NaN, res);
         }
 
@@ -22,15 +23,15 @@ namespace CourseApp.Tests
         public void TestTaskBNullMass()
         {
             var mass = new double[0];
-            var res = Program.TaskB(1,2,mass);
-            Xunit.Assert.Equal(mass,res);
+            var res = Program.TaskB(1, 2, mass);
+            Xunit.Assert.Equal(mass, res);
         }
 
         [Fact]
         public void TestTaskB()
         {
             var x = new double[] { 1.88, 2.26, 3.84, 4.55, -6.21 };
-            var res = Program.TaskB(0.8,0.4,x);
+            var res = Program.TaskB(0.8, 0.4, x);
             var expy = new double[] { 2.05703837959061, 2.16686308069159, 2.57116265691716, 2.72248916007313, 3.38484435329608 };
             for (int i = 0; i < 5; i++)
             {
