a0aae3a8f7dd91d3b4c1ccdd977768ecf022df58
Александр <@DESKTOP-6G1MCJ8>
2019-11-07 22:02:35 +0300

    Merge conflict

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -8,7 +8,6 @@ namespace CourseApp
 {
     public class Program
     {
-<<<<<<< HEAD
         public static double FuncthionZnach(double x)
         {
             double result = Math.Pow(Math.Abs(Math.Pow(x, 2) - 2.5), 0.25) + Math.Pow(Math.Log10(Math.Pow(x, 2)), 0.33);
@@ -29,29 +28,11 @@ namespace CourseApp
         }
 
         public static double[] FuncthionForMass(double[] x)
-=======
-        public static double MyFunction(double a, double b, double x)
-        {
-            var y = (a * Math.Pow(x, 2)) + (b * x);
-            return y;
-        }
-
-        public static double[] TaskA(double a, double b, double xn, double xk, double dx)
-        {
-            return new double[0];
-        }
-
-        public static double[] TaskB(double a, double b, double[] x)
->>>>>>> master
         {
             var y = new double[x.Length];
             for (var i = 0; i < x.Length; i++)
             {
-<<<<<<< HEAD
                 y[i] = FuncthionZnach(x[i]);
-=======
-                y[i] = MyFunction(a, b, x[i]);
->>>>>>> master
             }
 
             return y;
@@ -59,7 +40,6 @@ namespace CourseApp
 
         public static void Main(string[] args)
         {
-<<<<<<< HEAD
             Console.WriteLine($"Задание A:");
             foreach (double element in FuncthionForShag(1.25, 3.25, 0.4))
             {
@@ -74,20 +54,6 @@ namespace CourseApp
             }
 
             Console.WriteLine();
-=======
-            Console.WriteLine("Hello World!");
-            const double a = 2.2;
-            const double b = 3.8;
-            var resSingle = MyFunction(a, b, 4);
-            Console.WriteLine(resSingle);
-            var x = new double[] { 1, 2, 3, 4, 5 };
-            var taskBRes = TaskB(a, b, x);
-            foreach (var item in taskBRes)
-            {
-                Console.WriteLine($"y = {item}");
-            }
-
->>>>>>> master
             Console.ReadLine();
         }
     }
