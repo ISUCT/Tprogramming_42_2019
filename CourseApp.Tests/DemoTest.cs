197a197b022621dd11f526ff6536e7bc2822c3a2
User of A309 <a309@it.isuct.ru>
2019-11-09 13:47:33 +0400

    hotfix

--- CourseApp.Tests/DemoTest.cs
+++ CourseApp.Tests/DemoTest.cs
@@ -6,39 +6,33 @@ namespace CourseApp.Tests
     public class DemoTest
     {
         [Theory]
-        [InlineData(4.1, 2.7, 2.8, 20)]
-        [InlineData(4.1, 2.7, 3.6, 13)]
+        [InlineData(4.1, 2.7, 2.8, 20.109)]
+        [InlineData(4.1, 2.7, 3.6, 13.568)]
         public void TestEquat(double a, double b, double x, double exp)
         {
-            int res = (int)Math.Floor(Program.Equat(a, b, x));
-            Assert.Equal(res, exp);
+            Assert.Equal(Program.Equat(a, b, x), exp, 3);
         }
 
-        [Theory]
-        [InlineData(4.1, 2.7, 5.2, 5.1, 0.8)]
-        public void TestTaskA(double a, double b, double xn, double xk, double dx)
+        [Fact]
+        public void TestTaskA()
         {
-            var res = Program.TaskA(a, b, xn, xk, dx);
+            var res = Program.TaskA(4.1, 2.7, 5.2, 5.1, 0.8);
             Assert.Equal(res, new double[0]);
-
-            // Assert.Equal(exp, res, 3);
         }
 
-        [Theory]
-        [InlineData(4.1, 2.7)]
-        public void TestTaskB(double a, double b)
+        [Fact]
+        public void TestTaskB()
         {
             double[] e = new double[0];
-            var res = Program.TaskB(a, b, e);
+            var res = Program.TaskB(4.1, 2.7, e);
             Assert.Equal(res, new double[0]);
         }
 
-        [Theory]
-        [InlineData(4.1, 2.7)]
-        public void TestTaskBWork(double a, double b)
+        [Fact]
+        public void TestTaskBWork()
         {
             double[] mass = new double[5] { 1.9, 2.15, 2.34, 2.73, 3.16 };
-            var res = Program.TaskB(a, b, mass);
+            var res = Program.TaskB(4.1, 2.7, mass);
             int[] resMass = new int[mass.Length];
             for (int i = 0; i < mass.Length; i++)
             {
