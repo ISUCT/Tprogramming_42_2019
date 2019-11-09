2e917b2507b2422f9df8f4d5e8ef2ac95ab15652
User of A309 <a309@it.isuct.ru>
2019-11-09 15:02:06 +0400

    new proj classes

---
+++ БУДЬ_ТЫ_ПРОКЛЯТ_ПЕРРИ_УТКОНОС/Perry/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Perry
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
