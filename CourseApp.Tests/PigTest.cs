183f6af43302ca1d454ea435051f71b818d2e5ee
viktormurashov <murashovviktorv@gmail.com>
2019-11-11 09:25:53 +0400

    Добавил тесты для конструторов

--- CourseApp.Tests/PigTest.cs
+++ CourseApp.Tests/PigTest.cs
@@ -5,6 +5,35 @@ namespace CourseApp.Tests
 {
     public class PigTest
     {
+        [Theory]
+        [InlineData("Name", 1, 10)]
+        [InlineData("Name1", 3, 12320)]
+        public void TestConstructorThreeParametrs(string name, int age, int countSalo)
+        {
+            var item = new Pig(name, age, countSalo);
+            Assert.Equal(age, item.Age);
+            Assert.Equal(name, item.Name);
+            Assert.Equal(countSalo, item.CountSalo);
+        }
+
+        [Fact]
+        public void TestConstructorTwoParametrs()
+        {
+            var item = new Pig("Name2", 3);
+            Assert.Equal(3, item.Age);
+            Assert.Equal("Name2", item.Name);
+            Assert.Equal(0, item.CountSalo);
+        }
+
+        [Fact]
+        public void TestConstructorOneParametrs()
+        {
+            var item = new Pig("Name3");
+            Assert.Equal(0, item.Age);
+            Assert.Equal("Name3", item.Name);
+            Assert.Equal(0, item.CountSalo);
+        }
+
         [Fact]
         public void TestEmptyConstructor()
         {
