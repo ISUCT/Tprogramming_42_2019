109bc3fd1f42fd1c3883fef81d36638bcb544a63
Chernyshelly <andreycherni@gmail.com>
2019-12-13 17:33:58 +0300

    Start spell and char

---
+++ RPG_autoBattler/Char.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RPG_autoBattler
+{
+    public abstract class Char
+    {
+        private float maxhp;
+        private float maxmp;
+        private float curhp;
+        private float curmp;
+        private List<Spell> spells;
+
+        public Char()
+            {
+                Lvl = 1;
+            }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public int Str { get; set; }
+
+        public int Agi { get; set; }
+
+        public int Int { get; set; }
+
+        public int Lvl { get; set; }
+    }
+}
---
+++ RPG_autoBattler/Spell.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RPG_autoBattler
+{
+    public delegate void TrigReal(string triggerType, Char attacker, Char victim, float specVal);
+
+    public delegate void CastDel(Char caster, Char victim, float specVal);
+
+    public class Spell
+    {
+        private TrigReal trigger;
+        private CastDel cast;
+
+        public bool IsRanged { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public string Name { get; set; }
+
+        public float MPcost { get; set; }
+
+        public void Trigger(string triggerType, Char attacker, Char victim, float specVal)
+        {
+            trigger(triggerType, attacker, victim, specVal);
+        }
+
+        public void Cast(Char caster, Char victim, float specVal)
+        {
+            cast(caster, victim, specVal);
+        }
+    }
+}
