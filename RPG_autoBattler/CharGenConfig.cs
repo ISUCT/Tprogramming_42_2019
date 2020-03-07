7647e927ab38fed900a208e4a95790f4aa6145b3
dk306 <dk306@DK306-11>
2020-03-07 09:41:07 +0400

    New conf file+class

---
+++ RPG_autoBattler/CharGenConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RPG_autoBattler
+{
+    public class CharGenConfig
+    {
+        public int MinStrength { get; set; }
+
+        public int MaxStrength { get; set; }
+
+        public int minAgility { get; set; }
+
+        public int maxAgility { get; set; }
+
+        public int minIntelligence { get; set; }
+
+        public int maxIntelligence { get; set; }
+    }
+}
