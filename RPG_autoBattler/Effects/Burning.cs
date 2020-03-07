177609b0a4ea523d2435b54d28502860e5d05e50
dk306 <dk306@DK306-11>
2020-03-07 09:26:31 +0400

    (int) in spec val added

--- RPG_autoBattler/Effects/Burning.cs
+++ RPG_autoBattler/Effects/Burning.cs
@@ -10,7 +10,7 @@ namespace RPG_autoBattler
 
         public void Trigger(TriggerType triggerType, Character attacker, Character victim, float[] specVal)
         {
-            if ((triggerType == TriggerType.EndTurn) && (specVal[0] == 0) && (TurnsLeft > 0))
+            if ((triggerType == TriggerType.EndTurn) && ((int)specVal[0] == 0) && (TurnsLeft > 0))
             {
                 Console.WriteLine($"{attacker} is on fire! {TurnsLeft - 1} turns left!");
                 TurnsLeft--;
--- RPG_autoBattler/Effects/NinjaBleedEffect.cs
+++ RPG_autoBattler/Effects/NinjaBleedEffect.cs
@@ -10,7 +10,7 @@ namespace RPG_autoBattler
 
         public void Trigger(TriggerType triggerType, Character attacker, Character victim, float[] specValue)
         {
-            if ((triggerType == TriggerType.EndTurn) && (specValue[0] == 0) && (TurnsLeft > 0))
+            if ((triggerType == TriggerType.EndTurn) && ((int)specValue[0] == 0) && (TurnsLeft > 0))
             {
                 Console.WriteLine($"{attacker} is bleeding! {TurnsLeft} turns left!");
                 attacker.CurHP -= DamagePercent * attacker.MaxHP;
